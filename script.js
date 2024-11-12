f04d152ccb6cf151977ab9010951ffd733792c9b
marty957 <fantuzzimartina@yahoo.it>
2024-11-12 20:48:14 +0100

    third commit

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ const form = document.querySelector("form");
 
 const lastNameSaved = document.querySelector("label"); // lascio fuori la variabile perchè dovendo cambiare la stessa label ha piu senso, se la mettesse dentro la funzione mi crearebbe una label ad ogni click
 console.log(lastNameSaved);
-
-const names = []; //array vuoto
+const ul = document.getElementById("nameList");
+const names = localStorage.getItem("nameSaved") ? JSON.parse(localStorage.getItem("nameSaved")) : []; //array vuoto
 
 //funzione principale per il submit del form
-
+const createListItems = function () {
+  ul.innerHTML = "";
+  names.forEach((name) => {
+    const li = document.createElement("li");
+    li.innerText = name;
+    li.className = "list-group-item";
+    ul.appendChild(li);
+  });
+};
 form.onsubmit = function (e) {
   e.preventDefault();
   const name = document.querySelector("input");
   names.push(name.value); // creazione array
   console.log(names);
   localStorage.setItem("nameSaved", JSON.stringify(names)); // salvataggio in locale di ogni nome
-  const namesArray = JSON.parse(localStorage.getItem("nameSaved"));
-  namesArray.forEach((name) => {
-    lastNameSaved.innerText = name; //cambio della label con l'ultimo nome salvato
-  });
+  createListItems();
 };
 
 // tasto di reset
@@ -28,24 +33,24 @@ const btnReset = document.getElementById("reset");
 const toReset = function () {
   let resettinArray = JSON.parse(localStorage.getItem("nameSaved"));
 
-  resettinArray.forEach((name) => {
-    if (resettinArray.length > 0) {
-      resettinArray.pop();
-      localStorage.setItem("nameSaved", JSON.stringify(resettinArray));
-    }
-  });
+  resettinArray.pop();
+  localStorage.setItem("nameSaved", JSON.stringify(resettinArray));
+  ul.lastElementChild.remove();
 };
-
 btnReset.onclick = toReset;
-
 //creazione timer nel sessionstorage
 
 const timer = document.createElement("p");
-let i = 0;
+form.appendChild(timer);
+let i = sessionStorage.getItem("counter") ? parseInt(sessionStorage.getItem("counter")) : 0;
 
 const intervallo = setInterval(() => {
   i++;
   timer.innerText = i;
-  form.appendChild(timer);
-  sessionStorage.getItem("counter", JSON.stringify(i));
+
+  sessionStorage.setItem("counter", JSON.stringify(i));
 }, 1000);
+window.addEventListener("DOMContentLoaded", function () {
+  createListItems();
+  timer.innerText = i;
+});
